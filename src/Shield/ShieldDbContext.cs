34d7459011f49e2f58cb87ae5bb301a7506eeaf1
Danvic Wang <danvic96@hotmail.com>
2023-08-19 22:19:13 +0800

    Initialize the repository

---
+++ src/Shield/ShieldDbContext.cs
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------
+// <copyright file= "ShieldDbContext.cs">
+//     Copyright (c) Danvic.Wang All rights reserved.
+// </copyright>
+// Author: Danvic.Wang
+// Created DateTime: 2023-08-19 22:15
+// Modified by:
+// Description:
+// -----------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Shield;
+
+public class ShieldDbContext : DbContext
+{
+    
+}
